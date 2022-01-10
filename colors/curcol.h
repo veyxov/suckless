5563d90add73afef7d21ddf2ee20b581f2827291
Shekhov Ismoil <veyxov@gmail.com>
2022-01-10 21:14:24 +0500

    EMOJY patch!

--- colors/curcol.h
+++ colors/curcol.h
@@ -1,6 +1,5 @@
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#504945", "#222222" },
+    [SchemeSel] =  { "#fbf1c7", "#282C34" },
+    [SchemeOut] =  { "#eeeeee", "#FF0000" },
 };
