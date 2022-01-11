84d437219a337e5596a50fd22e1cd94895ef5629
Shekhov Ismoil <veyxov@gmail.com>
2022-01-11 07:37:15 +0500

    Fixed: Emjoies now work !

--- config.h
+++ config.h
@@ -4,9 +4,11 @@
 static int instant = 1;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"Fira code nerd font:size=10"
+static const char *fonts[]          = {
+    "Fira code nerd font:size=10",
+    "JoyPixels:pixelsize=16"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 #include "colors/curcol.h"
