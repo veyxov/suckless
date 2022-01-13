1f64984d887cfe43af8a377a7b147a47ff885a25
Shekhov Ismoil <veyxov@gmail.com>
2022-01-13 16:27:13 +0500

    Darker colors for the bar

--- colors/curcol.h
+++ colors/curcol.h
@@ -1,5 +1,5 @@
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#282C34";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#fbf1c7";
 static const char col_cyan[]        = "#282C34";
