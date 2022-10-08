bb239eee144fdc31a16e880de4fc97e61ef0eb20
Shekhov Ismoil <veyxov@gmail.com>
2022-10-08 17:43:51 +0500

    added DMENU

---
+++ Dmenu/colors/curcol.h
@@ -0,0 +1,5 @@
+static const char *colors[SchemeLast][2] = {
+    [SchemeNorm] = { "#fbf1c7", "#000000" },
+    [SchemeSel] =  { "#fbf1c7", "#282C34" },
+    [SchemeOut] =  { "#eeeeee", "#FF0000" },
+};
---
+++ Dmenu/colors/gruvbox.h
@@ -0,0 +1,5 @@
+static const char *colors[SchemeLast][2] = {
+    [SchemeNorm] = { "#ebdbb2", "#282828" },
+    [SchemeSel] =  { "#ebdbb2", "#98971a" },
+    [SchemeOut] =  { "#ebdbb2", "#8ec07c" },
+};
