952c32541c4cff5e9f1e5230d4e21d820a11b8ad
Shekhov Ismoil <veyxov@gmail.com>
2022-10-08 17:39:45 +0500

    Clean merge

--- colors/curcol.h
+++
@@ -1,5 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-    [SchemeNorm] = { "#fbf1c7", "#000000" },
-    [SchemeSel] =  { "#fbf1c7", "#282C34" },
-    [SchemeOut] =  { "#eeeeee", "#FF0000" },
-};
