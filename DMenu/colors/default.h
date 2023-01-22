bf33bd1e202442d2b079c1c35718323ace7c5116
Shekhov Ismoil <veyxov@gmail.com>
2023-01-22 17:51:50 +0500

    removed old files

--- DMenu/colors/default.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
--- DMenu/colors/gruvbox.h
+++
@@ -1,5 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-    [SchemeNorm] = { "#ebdbb2", "#282828" },
-    [SchemeSel] =  { "#ebdbb2", "#98971a" },
-    [SchemeOut] =  { "#ebdbb2", "#8ec07c" },
-};
