2a6bd3842c5d16bdec3cd4589e647dd676f302f1
Shekhov Ismoil <veyxov@gmail.com>
2023-02-27 21:22:02 +0500

    feat: added pinning keybind

--- dwm/config.h
+++ dwm/config.h
@@ -72,7 +72,7 @@ static const Key keys[] = {
 	// { MODKEY,                       XK_space,  setlayout,      {0} },
 	// { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	// { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_o,      tag,            {.ui = ~0 } },
+	{ MODKEY|ControlMask,             XK_o,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
